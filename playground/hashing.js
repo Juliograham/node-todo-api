22268a48952fba83bc95f108483f2d1132314cf2
Julio Graham <julioagraham@gmail.com>
2017-07-17 12:19:08 -0400

    Added hash to user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,23 +1,43 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 5
-};
+let password = '123456';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+let hashedPass = '$2a$10$VuaSNNpzK8OcchiCxTtoIOxSTI1n3YzzbqVYj.zVLWtZf5TIxDnPW';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+  console.log(res);
+});
 
 
 
 
-let token = jwt.sign(data, 'saltitup');
-let decoded = jwt.verify(token, 'saltitup');
 
-console.log(token);
-console.log(`Decoded: ${JSON.stringify(decoded)}`);
-// let message = 'I am user number one.';
-// let hash = SHA256(message).toString();
 
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
+// let data = {
+//     id: 5
+// };
+
+
+
+
+// let token = jwt.sign(data, 'saltitup');
+// let decoded = jwt.verify(token, 'saltitup');
+
+// console.log(token);
+// console.log(`Decoded: ${JSON.stringify(decoded)}`);
+// // let message = 'I am user number one.';
+// // let hash = SHA256(message).toString();
+
+// // console.log(`Message: ${message}`);
+// // console.log(`Hash: ${hash}`);
 
 // let data = {
 //   id: 4
